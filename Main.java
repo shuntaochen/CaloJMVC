21ec819301c594cb2ad9d6867652f77a095f3ebd
shuntao <359000081@qq.com>
2022-04-20 08:17:57 +0800

    String comparison use equals for object references, ==only applies to value type or memory address;

--- Main.java
+++ Main.java
@@ -5,23 +5,44 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import utils.*;
 
+import java.io.StringReader;
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.net.InetSocketAddress;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
 import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
 import core.*;
+import org.json.simple.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        String xx="{\"name\":\"\\名\"},\"a\"b:\"c \t\u0033称\"}";
+        byte[] bytes= xx.getBytes(StandardCharsets.UTF_8);
+        for (int i = 1; i < bytes.length; i++) {
+            int xxx=(int)bytes[i];
+            char xxxy=(char)xxx;
+            char xxxy1=(char)(bytes[i]<<4+bytes[i-1]);
+            char xxxy2='\t';
+        }
 
-        Object[] aa=new TestBean1[]{
-                new TestBean1("a"),
-                new TestBean1("b")
-        };
+        StringReader sr=new StringReader(xx);
+        int iii=0;
+        while (true){
+            int xy=sr.read();
+            char xz=(char)xy;
+            iii++;
+            if(iii==1000) break;
+        }
+
+//        Object[] aa=new TestBean1[]{
+//                new TestBean1("a"),
+//                new TestBean1("b")
+//        };
 
         new TypeChecker().Check();
         String json= new JsonHelper().convertToJson(new TestBean1("5"));
@@ -59,9 +80,20 @@ public class Main {
                         .getConstructors();
                 Controller ctrl = (Controller) constructors[0].newInstance(helper,propertyUtil);
                 int questionIndex=routeParts[1].indexOf("?");
-                Method method = ctrl.getClass().getDeclaredMethod(routeParts[1].substring(0,questionIndex!=-1?questionIndex:routeParts[1].length()));
-                method.invoke(ctrl);
-
+                String methodName= routeParts[1].substring(0,questionIndex!=-1?questionIndex:routeParts[1].length());
+                List<Method> methods= Arrays.asList(ctrl.getClass().getDeclaredMethods());
+                for (Method m:methods
+                     ) {
+                    String name=m.getName();
+                    boolean b= name.equals(methodName);
+                    boolean b1=name==methodName;//not applies to string,
+                    if(b){
+                        m.invoke(ctrl);
+                    }
+                    else{
+                        System.out.println(name);
+                    }
+                }
             }catch (ClassNotFoundException cnfe){
 
             }
