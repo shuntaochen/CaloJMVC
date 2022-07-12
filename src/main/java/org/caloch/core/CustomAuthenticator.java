f18a8d4406f4a793a7b3f466e00c43a001933186
shuntao <359000081@qq.com>
2022-07-12 15:08:47 +0800

    add custom principle;

--- src/main/java/org/caloch/core/CustomAuthenticator.java
+++ src/main/java/org/caloch/core/CustomAuthenticator.java
@@ -3,24 +3,31 @@ package org.caloch.core;
 
 import com.sun.net.httpserver.Authenticator;
 import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpPrincipal;
+
+import java.util.ArrayList;
 
 public class CustomAuthenticator extends Authenticator {
     @Override
     public Result authenticate(HttpExchange exch) {
         String[] perms = new String[]{PermissionNames.GeneralRead};
         String realm = String.join("|", permlize(perms));
-        return new Success(new HttpPrincipal("chen", realm));
+        JMvcPrinciple principal = new JMvcPrinciple("chen", realm);
+        ArrayList<String> feMenu = new ArrayList<>();
+        ArrayList<String> boMenu = new ArrayList<>();
+        principal.setFeMenu(feMenu);
+        principal.setBoMenu(boMenu);
+        return new Success(principal);
 //        return new Failure(500);
 //        return new Retry(301);//503,429,
     }
 
-    private String wrap(String src){
-        return "["+src+"]";
+    private String wrap(String src) {
+        return "[" + src + "]";
     }
-    private String[] permlize(String[] perms){
+
+    private String[] permlize(String[] perms) {
         for (int i = 0; i < perms.length; i++) {
-            perms[i]=wrap(perms[i]);
+            perms[i] = wrap(perms[i]);
         }
         return perms;
     }
---
+++ src/main/java/org/caloch/core/JMvcPrinciple.java
@@ -0,0 +1,39 @@
+package org.caloch.core;
+
+import com.sun.net.httpserver.HttpPrincipal;
+
+import java.util.List;
+
+public class JMvcPrinciple extends HttpPrincipal {
+    /**
+     * Creates a {@code HttpPrincipal} from the given {@code username} and
+     * {@code realm}.
+     *
+     * @param username the name of the user within the realm
+     * @param realm    the realm for this user
+     * @throws NullPointerException if either username or realm are {@code null}
+     */
+    public JMvcPrinciple(String username, String realm) {
+        super(username, realm);
+    }
+
+    private List<String> feMenu;
+    private List<String> boMenu;
+
+    public List<String> getFeMenu() {
+        return feMenu;
+    }
+
+    public void setFeMenu(List<String> feMenu) {
+        this.feMenu = feMenu;
+    }
+
+
+    public List<String> getBoMenu() {
+        return boMenu;
+    }
+
+    public void setBoMenu(List<String> boMenu) {
+        this.boMenu = boMenu;
+    }
+}
