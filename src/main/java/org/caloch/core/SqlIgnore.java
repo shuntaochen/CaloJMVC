fd334409148f4199f5ea601a556c6d6bd7f16467
shuntao <359000081@qq.com>
2022-07-18 18:19:02 +0800

    delete sqlignore annotation;

--- src/main/java/org/caloch/core/SqlIgnore.java
+++
@@ -1,8 +0,0 @@
-package org.caloch.core;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface SqlIgnore {
-}
