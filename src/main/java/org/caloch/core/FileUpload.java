784bccafa1bf56ae483302f83b1dd98404c95f3f
shuntao <359000081@qq.com>
2022-07-18 14:52:42 +0800

    tested 1st fileupload can be empty;

--- src/main/java/org/caloch/core/FileUpload.java
+++ src/main/java/org/caloch/core/FileUpload.java
@@ -7,5 +7,6 @@ import java.util.List;
 public class FileUpload extends FormDataHandle {
     @Override
     public void handle(HttpExchange httpExchange, List<MultiPart> parts) {//first file is always empty, less than actual amount,
+        //first fileupload can be empty, it is ok,
     }
 }
