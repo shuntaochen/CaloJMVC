65346f1c3cb2c8bd90097674c8b363e9415edda7
shuntao <359000081@qq.com>
2022-07-18 18:00:09 +0800

    refactor to frontend and backoffice;

--- src/main/java/org/caloch/controllers/BackOfficeSatisfact.java
+++ src/main/java/org/caloch/controllers/BackOfficeSatisfact.java
@@ -1,17 +1,22 @@
 package org.caloch.controllers;
 
+import org.caloch.core.Anonymous;
+import org.caloch.core.Permission;
+import org.caloch.core.PermissionNames;
 import org.caloch.core.Satisfact;
 import org.caloch.dtos.Login;
 import org.caloch.utils.CustomerContext;
 import org.caloch.utils.JwtUtil;
 import org.caloch.utils.PropertyUtil;
 
-public class LoginSatisfact extends Satisfact {
-    public LoginSatisfact(CustomerContext context, PropertyUtil properties, JwtUtil jwtUtil) {
+@Permission(name = PermissionNames.BackofficeAdmin)
+public class BackOfficeSatisfact extends Satisfact {
+    public BackOfficeSatisfact(CustomerContext context, PropertyUtil properties, JwtUtil jwtUtil) {
         super(context, properties, jwtUtil);
     }
 
-    public String in() {
+    @Anonymous
+    public String login() {
         Login dto = new Login();
         dto.name = request("name");
         dto.password = request("password");
