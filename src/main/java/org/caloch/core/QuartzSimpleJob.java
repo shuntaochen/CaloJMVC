7e9489289adadd1c95ad88e51b0d5e36dfd52d2a
shuntao <359000081@qq.com>
2022-07-20 18:01:41 +0800

    add logger to quartz job;

--- src/main/java/org/caloch/core/QuartzSimpleJob.java
+++ src/main/java/org/caloch/core/QuartzSimpleJob.java
@@ -1,17 +1,22 @@
 package org.caloch.core;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
+import java.util.Date;
+
 public class QuartzSimpleJob implements Job {
+    static Logger logger = Logger.getLogger(QuartzSimpleJob.class);
     public void execute(JobExecutionContext context) {
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
 
         String jobSays = dataMap.getString("jobSays");
         float myFloatValue = dataMap.getFloat("myFloatValue");
 
+        logger.info(jobSays+myFloatValue+":"+new Date().getTime());
         System.out.println("Job says: " + jobSays + ", and val is: " + myFloatValue);
     }
 }
