82f549cd3bb0902f07281201f703d86013ec0d7d
shuntao <359000081@qq.com>
2022-07-20 18:41:00 +0800

    update quartz dependencies;

--- src/main/java/org/caloch/core/QuartzScheduler.java
+++ src/main/java/org/caloch/core/QuartzScheduler.java
@@ -78,7 +78,7 @@ public class QuartzScheduler {
     private static Trigger createTrigger() {
 
         //initialize time interval
-        int TIME_INTERVAL = 5;
+        int TIME_INTERVAL = 60 * 60 * 6;
 
         //create a trigger to be returned from the method
         Trigger triggerNew = TriggerBuilder.newTrigger().withIdentity(NAME_OF_TRIGGER, NAME_OF_GROUP)
