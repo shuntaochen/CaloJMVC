e6314a814a6fcb4a4d170d1bd5e3f222de29ad39
shuntao <359000081@qq.com>
2022-07-21 18:24:37 +0800

    add sending http requests;

--- src/main/java/org/caloch/Main.java
+++ src/main/java/org/caloch/Main.java
@@ -14,5 +14,6 @@ public class Main {
                 .start();
         new QuartzScheduler().run();
 
+
     }
 }
---
+++ src/main/java/org/caloch/utils/HttpRequestor.java
@@ -0,0 +1,85 @@
+package org.caloch.utils;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HttpRequestor {
+
+    public static void main(String[] args) throws IOException {
+        URL url = new URL("https://www.youtube.com");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+
+        connection.setRequestMethod("POST");
+        Map<String, String> params = new HashMap<>();
+        params.put("v", "dQw4w9WgXcQ");
+
+        StringBuilder requestData = new StringBuilder();
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            if (requestData.length() != 0) {
+                requestData.append('&');
+            }
+            requestData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            requestData.append('=');
+            requestData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+
+        byte[] postDataBytes = requestData.toString().getBytes("UTF-8");
+        connection.setDoOutput(true);
+        try (DataOutputStream writer = new DataOutputStream(connection.getOutputStream())) {
+            writer.write(postDataBytes);
+
+            StringBuilder content;
+
+            try (BufferedReader in = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                content = new StringBuilder();
+                while ((line = in.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+            System.out.println(content.toString());
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    private void get() throws IOException {
+
+        URL url = new URL("https://www.youtube.com");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setConnectTimeout(10000);
+        connection.setReadTimeout(10000);
+
+        // To store our response
+        StringBuilder content;
+
+// Get the input stream of the connection
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line;
+            content = new StringBuilder();
+            while ((line = input.readLine()) != null) {
+                // Append each line of the response and separate them
+                content.append(line);
+                content.append(System.lineSeparator());
+            }
+        } finally {
+            connection.disconnect();
+        }
+
+// Output the content to the console
+        System.out.println(content.toString());
+    }
+}
