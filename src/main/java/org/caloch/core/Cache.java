4f0a35af74aa8d5ebc1175782f51c397ecf2ec23
shuntao <359000081@qq.com>
2022-07-23 09:57:18 +0800

    add a simple inmemory cache implementation;

---
+++ src/main/java/org/caloch/core/Cache.java
@@ -0,0 +1,15 @@
+package org.caloch.core;
+
+
+public interface Cache {
+
+    void add(String key, Object value, long periodInMillis);
+
+    void remove(String key);
+
+    Object get(String key);
+
+    void clear();
+
+    long size();
+}
