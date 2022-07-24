836464001628c1087b03187c7f558f4e7959ad66
shuntao <359000081@qq.com>
2022-07-24 11:44:27 +0800

    添加心得，readobject递归；

--- src/test/java/org/caloch/utils/StrJsonReaderTest.java
+++ src/test/java/org/caloch/utils/StrJsonReaderTest.java
@@ -9,6 +9,7 @@ public class StrJsonReaderTest {
 
     private String dest = "{\"bb\":5,\"ab\":\"a34b\\\\\\\"bsdf\"}";
     StringReader stringReader = new StringReader(dest);
+
     int counter = 0;
     //position: type:key, content: val,
 
@@ -23,7 +24,7 @@ public class StrJsonReaderTest {
     }
 
 //readJarry, if val type, read final, if object type, readObject, readtoken, read while !stream.end, return jarray, readsplitter,
-// readObject, 还是要递归， 然后readval,readstring,readarray,readjarray, 这里要read jobject ,直到jarray 结束 ，主要就是readObject,应该就会结束了，先不写了，要peek下一个
+// readObject, 还是要递归， 然后readval,readstring,readarray,readjarray, 这里要read jobject ,直到jarray 结束 ，主要就是readObject,应该就会结束了，先不写了，
 
     private char readBoundaryChar() throws IOException {
         while (true) {
